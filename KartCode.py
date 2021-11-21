ff4b2a698c5eaf7f28718f8e79194ebeaf92252b
xabnzk <93670973+xabnzk@users.noreply.github.com>
2021-11-21 23:30:44 +0300

    Add files via upload

---
+++ KartCode.py
@@ -0,0 +1,35 @@
+from getpass import getpass
+
+kullanıci = "Veysel Saki Çakır"
+Sifre = "abuzer"
+
+kullanici_giris_isim = input(" Kullanıcı Adı =>")
+kullanici_giris_sifre = getpass(" Kullanıcı Şifresi =>")
+
+if (kullanici_giris_isim == kullanıci and kullanici_giris_sifre == Sifre):
+    print("Hoşgeldin", kullanici_giris_isim)
+
+else:
+    print("Yanlış Giriş Yaptınız")
+
+print(" ")
+print(" ")
+print(" ")
+print(" ")
+print("Ana Menuye Hoş Geldinniz")
+
+print("Bilet Fiyatı 100₺")
+print("Öğrenciye -15₺ İndirim")
+
+alici_isim = input("İsim ->")
+alici_yas = input("Yaş ->")
+alici_durumu = input("Öğrencimi? ->")
+
+if alici_durumu == ("Evet"):
+    print(alici_isim,",Tutar 85₺")
+
+else:
+    print(alici_isim,",Tutar 100₺")
+
+while True:
+    pass
